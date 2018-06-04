fa93860bfa4da32827d30143bd706dba9d60d58e
Adam <23281721+nicecore@users.noreply.github.com>
2018-06-04 18:47:53 -0500

    Update 03_list_less_than_ten.py

--- PracticePython/03_list_less_than_ten.py
+++ PracticePython/03_list_less_than_ten.py
@@ -1,1 +1,12 @@
+import random
 
+a = random.sample(range(1, 10), 7)
+
+def less_than_five(sequence):
+  lessThanFive = []
+  for i in sequence:
+    if i < 5:
+      lessThanFive.append(i)
+  print(lessThanFive)
+
+less_than_five(a)
